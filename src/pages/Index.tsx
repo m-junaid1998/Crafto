bffa996c89ec16535d75f7ea53b4e46870179bc4
m-junaid1998 <muhammadjunaid.irfan1998@gmail.com>
2026-07-24 18:34:11 +0500

    home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,15 @@
+import { CategorySection } from "./CategorySection";
+import { Hero } from "./Hero";
+import { ProductList } from "./ProductList";
+
 const Home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <>
+      <Hero />
+      <CategorySection />
+      <ProductList />
+    </>
+  );
+};
 
-export default Home
+export default Home;
