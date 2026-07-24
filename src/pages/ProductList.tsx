99e6201bf5906d5dc800d5a3d627e151c3e20b02
m-junaid1998 <muhammadjunaid.irfan1998@gmail.com>
2026-07-24 18:46:07 +0500

    set grid col

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -4,7 +4,7 @@ import { sampleProducts } from '../utils/sampleProduct';
 export const ProductList = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {sampleProducts.map((prod) => (
           <ProductCard key={prod.id} product={prod} />
         ))}
