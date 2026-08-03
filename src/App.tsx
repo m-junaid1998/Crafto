319c9925dd80bba0c399e7da4269bdd659db37a6
m-junaid1998 <muhammadjunaid.irfan1998@gmail.com>
2026-08-03 20:15:52 +0500

    add scroll top

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import MainRouter from "./Routes";
-
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <MainRouter />
     </BrowserRouter>
   );
